6e30a4039a46b5f85bc422585d72e86aa1503015
Hforna <henriqueflashpingo@gmail.com>
2024-12-10 22:02:12 -0300

    creating use case for add tattoo to order

---
+++ src/backend/TatooMarket.Application/UseCases/Order/AddTattooToOrder.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper.Configuration.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Sqids;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatooMarket.Application.UseCases.Repositories.Order;
+using TatooMarket.Communication.Requests.Order;
+using TatooMarket.Domain.Repositories;
+using TatooMarket.Domain.Repositories.Orders;
+using TatooMarket.Domain.Repositories.Security.Token;
+
+namespace TatooMarket.Application.UseCases.Order
+{
+    public class AddTattooToOrder : IAddTattooToOrder
+    {
+        private readonly IOrderWriteOnly _orderWrite;
+        private readonly IOrderReadOnly _orderRead;
+        private readonly IGetUserByToken _userByToken;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly SqidsEncoder<long> _sqidsEncoder;
+
+        public async Task Execute(RequestAddTattooToOrder request, ISession session)
+        {
+            var user = await _userByToken.GetUser();
+
+            if (user == null)
+            {
+                if(session.TryGetValue("order", out var _))
+                {
+                    session.
+                }
+            }
+        }
+    }
+}
---
+++ src/backend/TatooMarket.Application/UseCases/Repositories/Order/IAddTattooToOrder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatooMarket.Communication.Requests.Order;
+
+namespace TatooMarket.Application.UseCases.Repositories.Order
+{
+    public interface IAddTattooToOrder
+    {
+        public Task Execute(RequestAddTattooToOrder request, ISession session);
+    }
+}
